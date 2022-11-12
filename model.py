7f212fc9e3c14e9dc6e508b73e5c9dd56abfb932
Caio Jordan <caioj2906@gmail.com>
2022-11-12 11:16:56 +0100

    5_128, lr step at 12-24 gamma 0.2 dropout

--- model.py
+++ model.py
@@ -37,7 +37,7 @@ class RetweetModel(LightningModule):
             hidden_layers_dict['layer' + str(i + 1)] = nn.Linear(self.layer_width, self.layer_width)
             hidden_layers_dict['relu' + str(i + 1)] = nn.ReLU()
             if self.dropout:
-                hidden_layers_dict['dropout' + str(i + 1)] = nn.Dropout()
+                hidden_layers_dict['dropout' + str(i + 1)] = nn.Dropout(p=0.2)
         self.hidden_layers = nn.Sequential(hidden_layers_dict)
         self.output = nn.Linear(self.layer_width, 1)
         self.relu = nn.ReLU()
