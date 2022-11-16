ef23576ec887b10578cbb949e0bdd773265407d5
Caio Jordan <caioj2906@gmail.com>
2022-11-16 13:45:48 +0100

    32 embed dims, one split only

--- run_training.py
+++ run_training.py
@@ -61,8 +61,10 @@ if __name__ == '__main__':
         # Trainer
         trainer = Trainer(accelerator='auto',
                           devices=1 if torch.cuda.is_available() else None,
-                          max_epochs=48,
+                          max_epochs=64,
                           val_check_interval=3000,
                           callbacks=[model_ckpt, lr_monitor],
                           logger=logger)
         trainer.fit(model, data_module)
+
+        break
