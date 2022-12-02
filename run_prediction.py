8e8c8915b03a6fac84e00696af4220c8da0d87c4
Caio Jordan <caioj2906@gmail.com>
2022-12-02 18:52:43 +0100

    round results

--- run_prediction.py
+++ run_prediction.py
@@ -55,7 +55,8 @@ if __name__ == '__main__':
         prediction = torch.cat(test_results).numpy()
         results.append(prediction)
 
-    final_predictions = np.array(results).mean(0)
+    #final_predictions = np.array(results).mean(0)
+    final_predictions = np.array(results).mean(0).around()
 
     test_results_df = pd.DataFrame(data={'retweets_count': final_predictions})
     test_ids = pd.DataFrame(data_module.test_ids)
